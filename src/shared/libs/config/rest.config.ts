9af5022e32f3f6bad85ef87a3f5cfec0e83fc72f
Igor Belyaev <IYBelyaev@alfabank.ru>
2024-01-30 23:28:55 +0300

    fix(*): adds  eslint fixes

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -8,25 +8,25 @@ import { Component } from '../../types/component.enum.js';
 
 @injectable()
 export class RestConfig implements Config<RestSchema> {
-    private readonly config: RestSchema;
+  private readonly config: RestSchema;
 
-    constructor(
+  constructor(
         @inject(Component.Logger) private readonly logger: Logger
-    ) {
-        const parsedOutput = config();
-    
-        if(parsedOutput.error) {
-            throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
-        }
-    
-        configRestSchema.load({});
-        configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
-    
-        this.config = configRestSchema.getProperties();
-        this.logger.info('.env file found and successfully parsed!');
+  ) {
+    const parsedOutput = config();
+
+    if(parsedOutput.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
     }
 
-    public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
-        return this.config[key];
-    }
+    configRestSchema.load({});
+    configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
+
+    this.config = configRestSchema.getProperties();
+    this.logger.info('.env file found and successfully parsed!');
+  }
+
+  public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
+    return this.config[key];
+  }
 }
